519b5c44bb47128eca222210e5c1aaee41170fe8
ElK <62822011+evkonradi@users.noreply.github.com>
2020-06-28 12:41:47 -0500

    final corrections

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,6 @@
 selectedCitiesEl = document.querySelector("#selectedCities");
 searchCityInputEl = document.querySelector("#searchCityInput");
+errorEl = document.querySelector("#error");
 var selectedCitiesAr = [];
 
 //Add a new city to the list of selected cities and save to Local Storage
@@ -26,12 +27,17 @@ var readCitiesList = function(){
         for (i = 0; i < selectedCitiesAr.length; i++){
             addCityToList(selectedCitiesAr[i]);
         }
-
     }
 }
 
+//Display error message
+var displayError = function(errorToDisplay, cityname){
+   if (!errorEl.innerHTML)
+        errorEl.innerHTML = "Error getting data for " + cityname + ": " + errorToDisplay;
+}
+
 //API to get current UV index
-var getCurrentUVIndex = function(lon, lat){
+var getCurrentUVIndex = function(lon, lat, cityName){
     var apiUrlVIndex = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=54f32668a2bf6aeb6c55df89a2e807fb";
 
     fetch(apiUrlVIndex).then(function(response) {
@@ -51,16 +57,19 @@ var getCurrentUVIndex = function(lon, lat){
                     elUVIndex.style.backgroundColor = "purple";
             });
         } else {
-            alert("Error: " + response.statusText);
+            displayError(response.statusText, cityName);
         }
     })
     .catch(function(error){
-        alert(error);
+        displayError("Unable to connect to the server.", cityname);
     });
 }
 
 //API to get current weather
-var getCurrentWeather = function(cityName){
+var getCurrentWeather = function(cityName, needToAddList){
+
+    errorEl.innerHTML="";
+
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=54f32668a2bf6aeb6c55df89a2e807fb";
   
     fetch(apiUrl).then(function(response) {
@@ -68,20 +77,27 @@ var getCurrentWeather = function(cityName){
             response.json().then(function(data) {
                 var containerCurWeather = document.getElementById("containerCurWeather");
                 containerCurWeather.className = containerCurWeather.className.replace(/\binvisible\b/g, "visible");
-                document.getElementById("cityNameWeather").innerHTML = cityName + "&nbsp;&nbsp;(" + moment().format("M/DD/YYYY") + ")";
+                document.getElementById("cityNameWeather").innerHTML = data.name + "&nbsp;&nbsp;(" + moment().format("M/DD/YYYY") + ")";
                 document.getElementById("currentWeatherIcon").src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
                 document.getElementById("temperature").innerHTML = data.main.temp + " °F";
                 document.getElementById("humidity").innerHTML = data.main.temp + " %";
                 document.getElementById("windSpeed").innerHTML = data.wind.speed + " MPH";
                 document.getElementById("UVIndex").innerHTML = data.main.temp;
-                getCurrentUVIndex(data.coord.lon, data.coord.lat);
+                getCurrentUVIndex(data.coord.lon, data.coord.lat, data.name);
+
+                //add city to the list and save to local storage
+                if (needToAddList && !selectedCitiesAr.includes(data.name)){
+                    addCityToList(data.name);
+                    addNewCityToLocalStorage(data.name);
+                }
+
             });
         } else {
-            alert("Error: " + response.statusText);
+            displayError(response.statusText, data.name);
         }
     })
     .catch(function(error){
-        alert(error);
+        displayError("Unable to connect to the server.", cityName);
     });
 }
 
@@ -94,47 +110,47 @@ var get5daysWeather = function(cityName){
             response.json().then(function(data) {
 
                 console.log(data);
-                
-                // get 3pm weather conditions
-                for (i=4; i< data.list.length; i=i+8){
 
-                    var container5DaysWeather = document.getElementById("container5DaysWeather");
-                    container5DaysWeather.className = container5DaysWeather.className.replace(/\binvisible\b/g, "visible");
+                var j = 1;
+                for (i=0; i< data.list.length; i++){
+                    // get 3pm weather conditions
+                    if (data.list[i].dt_txt.includes("15:00")){
+                        var container5DaysWeather = document.getElementById("container5DaysWeather");
+                        container5DaysWeather.className = container5DaysWeather.className.replace(/\binvisible\b/g, "visible");
+    
+                        var container5DaysHeading = document.getElementById("container5DaysHeading");
+                        container5DaysHeading.className = container5DaysHeading.className.replace(/\binvisible\b/g, "visible");
+    
+                        var cardEl = document.getElementById("card" + j );
+                        cardEl.className = "card-body bg-primary text-white rounded-sm";
+                        cardEl.innerHTML="";
+                        
+                        var h5El = document.createElement("h5");
+                        h5El.innerHTML= moment(data.list[i].dt_txt).format("MM/DD/YYYY");
+                        cardEl.appendChild(h5El);
+    
+                        var imgIcon = document.createElement("img");
+                        imgIcon.src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
+                        cardEl.appendChild(imgIcon);
+    
+                        var tempEl = document.createElement("div");
+                        tempEl.innerHTML = "Temp:&nbsp;&nbsp;" + data.list[i].main.temp + "°F";
+                        cardEl.appendChild(tempEl);
+    
+                        var humidityEl = document.createElement("div");
+                        humidityEl.innerHTML = "Humidity:&nbsp;&nbsp;" + data.list[i].main.humidity + "%";
+                        cardEl.appendChild(humidityEl);
 
-                    var container5DaysHeading = document.getElementById("container5DaysHeading");
-                    container5DaysHeading.className = container5DaysHeading.className.replace(/\binvisible\b/g, "visible");
-
-                    var cardEl = document.getElementById("card" + ((i-4)/8+1) );
-                    cardEl.className = "card-body bg-primary text-white rounded-sm";
-                    
-                    var h5El = document.createElement("h5");
-                    h5El.innerHTML= moment(data.list[i].dt_txt).format("MM/DD/YYYY");
-                    cardEl.appendChild(h5El);
-
-                    var imgIcon = document.createElement("img");
-                    imgIcon.src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
-                    cardEl.appendChild(imgIcon);
-
-                    var tempEl = document.createElement("div");
-                    tempEl.innerHTML = "Temp:&nbsp;&nbsp;" + data.list[i].main.temp + "°F";
-                    cardEl.appendChild(tempEl);
-
-                    var humidityEl = document.createElement("div");
-                    humidityEl.innerHTML = "Humidity:&nbsp;&nbsp;" + data.list[i].main.humidity + "%";
-                    cardEl.appendChild(humidityEl);
-
+                        j++;
+                    }
                 }
-                //document.getElementById("cityNameWeather").innerHTML = cityName + "&nbsp;&nbsp;(" + moment().format("M/DD/YYYY") + ")";
-                //document.getElementById("currentWeatherIcon").src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
-                //document.getElementById("temperature").innerHTML = data.main.temp + " F";
-                //document.getElementById("humidity").innerHTML = data.main.temp + " %";
             });
         } else {
-            alert("Error: " + response.statusText);
+            displayError(response.statusText, cityName);
         }
     })
     .catch(function(error){
-        alert(error);
+        displayError("Unable to connect to the server.", cityName);
     });
 }
 
@@ -143,22 +159,11 @@ var searchCity = function(){
     if (!searchCityInputEl.value){
         return;
     }
-
-    //capitalize the first letter
-    var cityName = searchCityInputEl.value.toLowerCase().trim();
-    cityName = cityName.charAt(0).toUpperCase() + cityName.slice(1);
+    var cityName = searchCityInputEl.value.trim();
 
     //get current weather (APi call)
-    getCurrentWeather(cityName);
-
+    getCurrentWeather(cityName, true);
     get5daysWeather(cityName);
-
-    //add city to the list and save to local storage
-    if (!selectedCitiesAr.includes(cityName))
-    {
-        addCityToList(cityName);
-        addNewCityToLocalStorage(cityName);
-    }
 
     searchCityInputEl.value = "";
 }
@@ -167,7 +172,7 @@ var searchCity = function(){
 var selectCityFromList = function(event){
     var targetEl = event.target;
     var cityName = event.target.getAttribute("data-city");
-    getCurrentWeather(cityName);
+    getCurrentWeather(cityName, false);
     get5daysWeather(cityName);
 }
 
--- README.md
+++ README.md
@@ -1,44 +1,21 @@
-# 06 Server-Side APIs: Weather Dashboard
+# Weather Dashboard
 
-Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+Weather Dashboard allows to see the current weather and 5 days forecast for a selected city.
+If you enter the city name and click search, weather information gets populated. The city is also added to the list of 
+selected citites which is saved to Local Storage.
+Alternatively you can click on the city in the list that contains previously searched cities. This list gets populated from the Local Storage.
 
-Use the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. The documentation includes a section called "How to start" that will provide basic setup and usage instructions. Use `localStorage` to store any persistent data.
+It is built by using HTML, JavaScript, Bootstrap and Server-Side APIs.
 
-## User Story
+# Deployed web site
 
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
+The link to deployed site is https://evkonradi.github.io/WeatherDashboard/.
 
-## Acceptance Criteria
+# GitHub repository
 
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-```
+You can find the repository here: https://github.com/evkonradi/WeatherDashboard.
 
-The following image demonstrates the application functionality:
+# Page preview
 
-![weather dashboard demo](./Assets/06-server-side-apis-homework-demo.png)
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+This is the preview of the initial page:
+<img src="./Assets/img/PagePreview.JPG">
